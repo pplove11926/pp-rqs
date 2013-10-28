46589580f16ff32334ca32f8a06ed743a2db7fcf
YI Peipei <yipeipei@gmail.com>
2013-10-28 22:19:42 +0800

    back home

--- src/com/yipeipei/algs/Hop.java
+++ src/com/yipeipei/algs/Hop.java
@@ -1,10 +1,24 @@
 package com.yipeipei.algs;
 
+import edu.princeton.cs.introcs.In;
+
 /**
  * 
  * @author peipei
  *
  */
 public class Hop {
+    private int V;
+    private int size;
     
+    public Hop(In in){
+        
+    }
+    
+    public static boolean verify(In hopIn, In tcIn){
+        TC tc = TC.load(tcIn);
+        Hop 
+        
+        return false;
+    }
 }
