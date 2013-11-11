d2adcae9a87490c69cd5b43ca8fcbd26a39f6c0c
Peipei YI <yipeipei@gmail.com>
2013-11-11 16:28:00 +0800

    topological sort, guarantee a DAG

--- src/com/yipeipei/algs/MinimalBoxCover.java
+++ src/com/yipeipei/algs/MinimalBoxCover.java
@@ -8,16 +8,33 @@ package com.yipeipei.algs;
  */
 public class MinimalBoxCover {
     private final TC tc;
-    private final boolean allowOverlap;
+    private final boolean allowOverlap; // allow overlapping might have more compression
+    private final int[][] aux;
     
     public MinimalBoxCover(TC tc, boolean allowOverlap){
         this.tc = tc;
         this.allowOverlap = allowOverlap;
+        
+        this.aux = new int[tc.getV()][tc.getV()];   // default 0
+        
         boxCover();
     }
     
     private void boxCover(){
+        // first pass, scan by column
+        for(int j = 0; j < this.tc.getV(); j++){
+            for(int i = this.tc.getV() -1 ; i >= 0; i++){
+                if(this.tc.matrix[i][j] == true){
+                    if(i == this.tc.getV() - 1){
+                        this.aux[i][j] = 1;
+                    }else{
+                        this.aux[i][j] = this.aux[i+1][j] + 1;
+                    }
+                }
+            }
+        }
         
+        // second pass, scan by row, identify maximal box and log the accumulate dummy factor
     }
     
 }
--- test/com/yipeipei/algs/TCTest.java
+++ test/com/yipeipei/algs/TCTest.java
@@ -8,29 +8,36 @@ import org.junit.Test;
 
 import com.yipeipei.pprqs.Data;
 
+import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.introcs.In;
 import edu.princeton.cs.introcs.StdOut;
 
 public class TCTest {
 
-    @Test
-    public void testFlip() {
-        In in = new In(Data.getFiles(Data.DATA_TEST, ".tc")[0]);
-        TC tc = TC.load(in);
-
-        StdOut.println(tc.toString());
-        
-        tc.flip(0, 0);
-        tc.flip(0, 1);
-        StdOut.print(tc.toString());
-        
-        assertEquals(tc.matrix[0][0], false);
-    }
+//    @Test
+//    public void testFlip() {
+//        File f = Data.getFiles(Data.DATA_TEST, ".tc")[0];
+//        StdOut.println(f.getAbsolutePath());
+//        In in = new In(f);
+//        TC tc = TC.load(in);
+//
+//        StdOut.println(tc.toString());
+//        
+//        tc.flip(0, 0);
+//        tc.flip(0, 1);
+//        StdOut.print(tc.toString());
+//        
+//        assertEquals(tc.matrix[0][0], false);
+//    }
     
     @Test
     public void testTopo(){
         File[] files = Data.getFiles(Data.DATA_TEST, ".topo");
-        
+        for(File f : files){
+            Digraph g = new Digraph(new In(f));
+            TC tc = new TC(g).minus();
+            StdOut.println(tc.toString());
+        }
     }
 
 }
