f93093bc443ad385d763b9cbbee1e2035dc1087c
Peipei YI <yipeipei@gmail.com>
2013-11-26 16:02:56 +0800

    remove paper/ and related tex files

--- paper/reachability_privacy_biclique/compile.bat
+++
@@ -1,7 +0,0 @@
-latex main.tex 
-
-dvips -Ppdf -t letter main.dvi -o  
-
-ps2pdf main.ps main.pdf
-
-start main.pdf -reuse-instance
--- paper/reachability_privacy_biclique/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-latex main.tex ; dvips -Ppdf -t letter main.dvi -o ; ps2pdf main.ps main.pdf
